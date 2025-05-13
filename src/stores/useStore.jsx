dc3e35fa1234246cd0d67d73ac285a09797fe8f4
Ethan Tran <khoitranec@gmail.com>
2025-05-13 20:41:47 +0700

    Adding persist on the state store

--- src/stores/useStore.jsx
+++ src/stores/useStore.jsx
@@ -1,10 +1,18 @@
 import { create } from "zustand";
 import fetchItemsSlice from "./slices/fetchItemsSlice";
 import cartSlice from "./slices/cartSlice";
+import { persist } from "zustand/middleware";
 
-const useStore = create((set) => ({
-  ...fetchItemsSlice(set),
-  ...cartSlice(set),
-}));
+const useStore = create(
+  persist(
+    (set) => ({
+      ...fetchItemsSlice(set),
+      ...cartSlice(set),
+    }),
+    {
+      name: "shoping-card",
+    },
+  ),
+);
 
 export default useStore;
