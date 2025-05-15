3bb99fbb4f4fa3ac12c3d64ab821feb794337035
Ethan Tran <khoitranec@gmail.com>
2025-05-15 21:49:27 +0700

    Adding hover for nav link

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -17,13 +17,13 @@ export default function Root() {
         </Link>
 
         <div className="flex gap-4 p-4 pr-0">
-          <Link to="/" className="px-3 py-2 text-xl">
+          <Link to="/" className="px-3 py-2 text-xl hover:text-gray-600">
             Home
           </Link>
-          <Link to="shop" className="px-3 py-2 text-xl">
+          <Link to="shop" className="px-3 py-2 text-xl hover:text-gray-600">
             Shop
           </Link>
-          <Link to="cart" className="px-3 py-2 text-xl">
+          <Link to="cart" className="px-3 py-2 text-xl hover:text-gray-600">
             <p
               key={getTotalItem}
               className="absolute top-4 right-21 flex h-[20px] min-w-[20px] animate-[bounce_1s_ease-in-out_forwards] items-center justify-center rounded-full bg-red-700 p-2 text-xs text-white"
