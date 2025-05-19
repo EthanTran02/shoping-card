e075b37ebbad264e9e35a5218e1864383901333e
Ethan Tran <khoitranec@gmail.com>
2025-05-19 21:51:37 +0700

    Decrease home page background image size

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
       className="-mx-20 -my-8 flex min-h-screen flex-col items-center justify-center bg-cover bg-center"
       style={{
         backgroundImage:
-          "url('https://images.unsplash.com/photo-1523381210434-271e8be1f52b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+          "url('https://images.unsplash.com/photo-1523381210434-271e8be1f52b?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
       }}
     >
       <div className="mx-auto -mt-15 flex w-full max-w-2xl flex-col items-center rounded-xl bg-white/70 px-8 py-10 backdrop-blur-sm">
